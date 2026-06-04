942f5e17a3f390e0dec92a640700b39926e65dbd
Pooja Tambe <poojatambe522@gmail.com>
2026-06-04 12:16:25 +0530

    new commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "rg" {
+  name = "rg-vnet"
+  location = "centralus"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name = "dlink"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  address_space = [ "10.0.0.0/16" ]
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name = "fronsubnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = [ "10.0.1.0/24" ]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name = "backsubnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = [ "10.0.2.0/24" ]
+}

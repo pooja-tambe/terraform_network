1e0a52f001baf02099de55e263d65764825520c8
Pooja Tambe <poojatambe522@gmail.com>
2026-06-04 12:19:39 +0530

    added new subnet

--- main.tf
+++ main.tf
@@ -22,3 +22,10 @@ resource "azurerm_subnet" "subnet2" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes = [ "10.0.2.0/24" ]
 }
+
+resource "azurerm_subnet" "subnet3" {
+  name = "backsubnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = [ "10.0.3.0/24" ]
+}
